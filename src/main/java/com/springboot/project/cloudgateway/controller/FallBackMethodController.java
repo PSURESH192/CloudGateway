24b860a811571a01f79d307805c36fff7b5a6dfd
Suresh_Prabha <suresh.uday92@gmail.com>
2021-06-19 15:53:46 +0530

    Swagger

--- src/main/java/com/springboot/project/cloudgateway/controller/FallBackMethodController.java
+++ src/main/java/com/springboot/project/cloudgateway/controller/FallBackMethodController.java
@@ -1,6 +1,7 @@
 package com.springboot.project.cloudgateway.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,18 +10,21 @@ public class FallBackMethodController {
 
     @CircuitBreaker(name = "hotelServiceFallBackMethod", fallbackMethod = "hotelServiceFallBackMethod")
     @GetMapping("/hotelServiceFallBackMethod")
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public String hotelServiceFallBackMethod(Exception e){
         return "Hotel Service took longer time than expected.Please try again after sometime";
     }
 
     @CircuitBreaker(name = "guestServiceFallBackMethod", fallbackMethod = "guestServiceFallBackMethod")
     @GetMapping("/guestServiceFallBackMethod")
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public String guestServiceFallBackMethod(Exception e){
         return "Guest Service took longer time than expected.Please try again after sometime";
     }
 
     @CircuitBreaker(name = "reservationServiceFallBackMethod", fallbackMethod = "reservationServiceFallBackMethod")
     @GetMapping("/reservationServiceFallBackMethod")
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public String reservationServiceFallBackMethod(Exception e){
         return "Reservation Service took longer time than expected.Please try again after sometime";
     }
--- src/main/java/com/springboot/project/cloudgateway/controller/ResourceController.java
+++ src/main/java/com/springboot/project/cloudgateway/controller/ResourceController.java
@@ -1,6 +1,7 @@
 package com.springboot.project.cloudgateway.controller;
 
 import com.springboot.project.cloudgateway.model.Message;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,21 @@ public class ResourceController {
 
     @GetMapping("/resource/user")
     @PreAuthorize("hasRole('USER')")
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public Mono<ResponseEntity<?>> user() {
        return Mono.just(ResponseEntity.ok(new Message("Content for user")));
     }
 
-   @GetMapping("/resource/admin")
+    @GetMapping("/resource/admin")
     @PreAuthorize("hasRole('ADMIN')")
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public Mono<ResponseEntity<?>> admin() {
         return Mono.just(ResponseEntity.ok(new Message("Content for admin")));
     }
 
     @GetMapping("/resource/user-or-admin")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class, example = "Bearer access_token")
     public Mono<ResponseEntity<?>> userOrAdmin() {
         return Mono.just(ResponseEntity.ok(new Message("Content for user or admin")));
     }
